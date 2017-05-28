b5d9a59fc3778200250d0cce7daf167bc7280c03
AdriDelegado <adrielpt91@gmail.com>
2017-05-28 17:29:24 +0100

    game and Map

--- SnakeClient/Map.h
+++ SnakeClient/Map.h
@@ -4,9 +4,23 @@
 #include <Windows.h>
 #include <string.h>
 #include <tchar.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
 
 typedef struct {
 	DWORD height;
 	DWORD width;
 	DWORD **tiles;
+	vector<int> map;
 }MAP;
+
+void inicialMap(DWORD height, DWORD width);
+
+boolean checkPos(DWORD pos);
+
+boolean setItem(DWORD pos);
+
+int convertCoordenatesIntoPos(int linhas, int colunas);
--- SnakeServer/Game.h
+++ SnakeServer/Game.h
@@ -7,26 +7,11 @@
 #include <tchar.h>
 
 typedef struct {
-	DWORD max_number_players;
+	DWORD max_number_players[20];
 
 	char max_number_items;
 	DWORD started_snake_size;
 	DWORD game_duration;
 	DWORD probability_rare_items;
 	DWORD number_ai_snakes;
-	DWORD list_of_players[20];
 }GAME;
-
-//
-//void settingDefaultValues(
-//	DWORD max_number_players,
-//	DWORD max_number_items,
-//	DWORD started_snake_size,
-//	DWORD game_duration,
-//	DWORD probability_rare_items,
-//	DWORD number_ai_snakes
-//);
-
-void serverInicializaGame(GAME *game);
-
-void mostraSettingFromClient(GAME *game);
